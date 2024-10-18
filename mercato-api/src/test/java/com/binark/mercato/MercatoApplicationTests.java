e8eb8b431241fa3be60dc6a5b1d2a1d6292f80f1
= <=>
2024-10-18 20:34:38 +0200

    Project initialization

---
+++ mercato-api/src/test/java/com/binark/mercato/MercatoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.binark.mercato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class MercatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mercato-api/src/test/java/com/binark/mercato/TestMercatoApplication.java
@@ -0,0 +1,11 @@
+package com.binark.mercato;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestMercatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(MercatoApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
